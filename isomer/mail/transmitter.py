90dc77aa6092da9a79c394f79738415f44bf463b
ri0t <riot@c-base.org>
2020-05-27 20:24:38 +0200

    Started on sendmail support, minor fixes & cleanups

--- isomer/mail/transmitter.py
+++ isomer/mail/transmitter.py
@@ -17,9 +17,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from isomer.database import objectmodels
-from isomer.debugger import cli_register_event
-from isomer.logger import verbose, error
+
 
 __author__ = "Heiko 'riot' Weinen"
 __license__ = "AGPLv3"
@@ -33,19 +31,24 @@ Module MailTransmitter
 
 """
 
-from circuits import Timer, Event, Worker, task
 from email.mime.text import MIMEText
 from smtplib import SMTP, SMTP_SSL
 from socket import timeout
-from pystache import render
 
+from circuits import Event, Worker, task
 from isomer.component import ConfigurableComponent, handler
-
+from isomer.database import objectmodels
+from isomer.debugger import cli_register_event
+from isomer.logger import verbose, error
 from isomer.mail import send_mail
+from isomer.schemata.defaultform import fieldset
+from isomer.tool import run_process
+from pystache import render
 
 
 def send_mail_worker(config, mail, event):
-    """Worker task to send out an email, which is a blocking process unless it is threaded"""
+    """Worker task to send out an email, which is a blocking process unless
+     it is threaded"""
     log = ""
 
     try:
@@ -120,32 +123,32 @@ class MailTransmitter(ConfigurableComponent):
                         'type': 'string',
                         'title': 'Mail server',
                         'description': 'Mail server to send emails from',
-                        'default': 'localhost'
+                        'default': 'localhost',
                     },
                     'port': {
                         'type': 'integer',
                         'title': 'Mail server port',
                         'description': 'Mail server port to connect to',
-                        'default': 465
+                        'default': 465,
                     },
                     'ssl': {
                         'type': 'boolean',
                         'title': 'Use SSL',
                         'description': 'Use SSL to secure the mail server connection',
-                        'default': True
+                        'default': True,
                     },
                     'tls': {
                         'type': 'boolean',
                         'title': 'Use TLS',
                         'description': 'Use TLS to secure the mail server connection',
-                        'default': False
+                        'default': False,
                     },
                     'protocol': {
                         'type': 'string',
                         'title': 'Server protocol',
                         'description': 'Protocol to use with this mail server',
                         'default': 'smtp',
-                        'enum': ['smtp']
+                        'enum': ['smtp'],
                     },
                     'mail_from': {
                         'type': 'string',
@@ -157,15 +160,34 @@ class MailTransmitter(ConfigurableComponent):
                     'username': {
                         'type': 'string',
                         'title': 'SMTP Username',
-                        'default': ''
+                        'default': '',
                     },
                     'password': {
                         'type': 'string',
                         'title': 'SMTP Password',
                         'x-schema-form': {
-                            'type': 'password'
+                            'type': 'password',
                         }
                     },
+                    'use_sendmail': {
+                        'type': 'boolean',
+                        'title': 'Use sendmail',
+                        'default': False,
+                        'description': 'Use local sendmail (e.g. msmtp) instead of smtp'
+                    },
+                    'sendmail_extra_arguments': {
+                        'type': 'string',
+                        'title': 'Sendmail arguments',
+                        'description':
+                            'Use these extra arguments to control e.g. msmtp',
+                        'default': '-t -oi'
+                    },
+                    'sendmail_binary': {
+                        'type': 'string',
+                        'title': 'Sendmail binary to use',
+                        'description': 'Specify the executable to send mail with',
+                        'default': '/usr/bin/sendmail'
+                    }
                 }
             }
         }
@@ -182,14 +204,26 @@ class MailTransmitter(ConfigurableComponent):
             },
             'items': [
                 'accounts[].name',
-                'accounts[].server',
-                'accounts[].port',
-                'accounts[].ssl',
-                'accounts[].tls',
-                'accounts[].protocol',
                 'accounts[].mail_from',
-                'accounts[].username',
-                'accounts[].password'
+                'accounts[].use_sendmail',
+                fieldset('Sendmail details', [
+                    'accounts[].sendmail_extra_arguments',
+                    'accounts[].sendmail_binary',
+                ], options={
+                    'condition': '$ctrl.model.accounts[arrayIndex].use_sendmail == true'
+                }),
+                fieldset('SMTP account details', [
+                    'accounts[].server',
+                    'accounts[].port',
+                    'accounts[].ssl',
+                    'accounts[].tls',
+                    'accounts[].protocol',
+                    'accounts[].username',
+                    'accounts[].password'
+                ], options={
+                    'condition':
+                        '$ctrl.model.accounts[arrayIndex].use_sendmail == false'
+                })
             ]
         },
     ]
@@ -197,7 +231,8 @@ class MailTransmitter(ConfigurableComponent):
     def __init__(self, *args, **kwargs):
         super(MailTransmitter, self).__init__('MAILTX', *args, **kwargs)
 
-        self.worker = Worker(process=False, workers=2, channel="mail-transmit-workers").register(self)
+        self.worker = Worker(process=False, workers=2,
+                             channel="mail-transmit-workers").register(self)
 
         self.hostname = objectmodels['systemconfig'].find_one({'active': True}).hostname
 
@@ -208,7 +243,8 @@ class MailTransmitter(ConfigurableComponent):
     @handler('cli_test_mail')
     def cli_mail_test(self, event):
         self.log('Sending testmail')
-        self.fireEvent(send_mail('riot@localhost', 'Testmail', 'Hello dear test mail receiver!'))
+        self.fireEvent(
+            send_mail('root@localhost', 'Testmail', 'Hello dear test mail receiver!'))
 
     def send_mail(self, event):
         """Connect to mail server and send actual email"""
@@ -221,18 +257,34 @@ class MailTransmitter(ConfigurableComponent):
         else:
             account_name = event.account
 
-        account = list(filter(lambda account: account['name'] == account_name, self.config.accounts))[0]
+        account = list(filter(lambda account: account['name'] == account_name,
+                              self.config.accounts))[0]
 
-        mime_mail['From'] = render(account['mail_from'], {'server': account['server'], 'hostname': self.hostname})
+        mime_mail['From'] = render(account['mail_from'], {
+            'server': account['server'],
+            'hostname': self.hostname
+        })
         mime_mail['To'] = event.to_address
 
         self.log('MimeMail:', mime_mail, lvl=verbose)
         if self.config.mail_send is True:
             self.log('Sending mail to', event.to_address)
 
-            self.fireEvent(task(send_mail_worker, account, mime_mail, event), "mail-transmit-workers")
+            if account['use_sendmail']:
+                # TODO: TEST THIS!!!
+                self.log('Would now task sendmail to send the mail')
+                result, log = run_process(
+                    '', [
+                            account['sendmail_binary']
+                        ] + account['sendmail_extra_arguments'].split(' '),
+                    stdin=mime_mail.as_string()
+                )
+            else:
+                self.fireEvent(task(send_mail_worker, account, mime_mail, event),
+                               "mail-transmit-workers")
         else:
-            self.log('Not sending mail, here it is for debugging info:', mime_mail, pretty=True)
+            self.log('Not sending mail, here it is for debugging info:', mime_mail,
+                     pretty=True)
 
     @handler('task_success', channel="mail-transmit-workers")
     def task_success(self, event, call, result):
